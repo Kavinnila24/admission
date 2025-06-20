120c2d4238a24a200f1b3e5c92f1e85a0a50a97f
Mansha <mansha02072003@gmail.com>
2025-06-20 15:45:41 +0530

    address checkbox

--- sApp1/src/components/resources/CreateCurrentaddress.tsx
+++ sApp1/src/components/resources/CreateCurrentaddress.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import apiConfig from '../../config/apiConfig';
+import React, { useState, useEffect } from "react";
+import apiConfig from "../../config/apiConfig";
 
 export type resourceMetaData = {
   resource: string;
@@ -11,10 +11,13 @@ const CreateCurrentaddress = () => {
   const [fields, setFields] = useState<any[]>([]);
   const [dataToSave, setDataToSave] = useState<any>({});
   const [showToast, setShowToast] = useState<any>(false);
-  const [foreignkeyData, setForeignkeyData] = useState<Record<string, any[]>>({});
-  const [searchQueries, setSearchQueries] = useState<Record<string, string>>({});
+  const [foreignkeyData, setForeignkeyData] = useState<Record<string, any[]>>(
+    {}
+  );
+  const [searchQueries, setSearchQueries] = useState<Record<string, string>>(
+    {}
+  );
   const [enums, setEnums] = useState<Record<string, any[]>>({});
-
   const regex = /^(g_|archived|extra_data)/;
   const apiUrl = apiConfig.getResourceUrl("currentaddress");
   const metadataUrl = apiConfig.getResourceMetaDataUrl("Currentaddress");
@@ -25,42 +28,42 @@ const CreateCurrentaddress = () => {
     state: "State",
     country: "Country",
     pincode: "Pincode",
+    city: "City",
   };
-
-  //Automatically set applicantid to "nil"
-  useEffect(() => {
-    setDataToSave((prev: any) => ({
-      ...prev,
-      applicantid: "nil",
-    }));
-  }, []);
-
   // Fetch metadata
   useEffect(() => {
     const fetchResMetaData = async () => {
       const fetchedResources = new Set();
       const fetchedEnum = new Set();
-
+      console.log("fectched resources", fetchedResources);
       try {
         const data = await fetch(metadataUrl, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
         });
 
         if (data.ok) {
           const metaData = await data.json();
           setResMetaData(metaData);
           setFields(metaData[0].fieldValues);
-
-          const foreignFields = metaData[0].fieldValues.filter((field: any) => field.foreign);
+          const foreignFields = metaData[0].fieldValues.filter(
+            (field: any) => field.foreign
+          );
+          console.log("foreign fields", foreignFields);
           for (const field of foreignFields) {
             if (!fetchedResources.has(field.foreign)) {
               fetchedResources.add(field.foreign);
-              await fetchForeignData(field.foreign, field.name, field.foreign_field);
+              await fetchForeignData(
+                field.foreign,
+                field.name,
+                field.foreign_field
+              );
             }
           }
 
-          const enumFields = metaData[0].fieldValues.filter((field: any) => field.isEnum === true);
+          const enumFields = metaData[0].fieldValues.filter(
+            (field: any) => field.isEnum === true
+          );
           for (const field of enumFields) {
             if (!fetchedEnum.has(field.possible_value)) {
               fetchedEnum.add(field.possible_value);
@@ -68,10 +71,10 @@ const CreateCurrentaddress = () => {
             }
           }
         } else {
-          console.error('Failed to fetch components:', data.statusText);
+          console.error("Failed to fetch components:", data.statusText);
         }
       } catch (error) {
-        console.error('Error fetching components:', error);
+        console.error("Error fetching components:", error);
       }
     };
 
@@ -81,40 +84,47 @@ const CreateCurrentaddress = () => {
   useEffect(() => {
     console.log("data to save", dataToSave);
   }, [dataToSave]);
-
   const fetchEnumData = async (enumName: string) => {
     try {
       const response = await fetch(`${apiConfig.API_BASE_URL}/${enumName}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
       });
 
       if (response.ok) {
         const data = await response.json();
         setEnums((prev) => ({
           ...prev,
-          [enumName]: data
+          [enumName]: data,
         }));
       } else {
-        console.error(`Error fetching enum data for ${enumName}:`, response.status);
+        console.error(
+          `Error fetching enum data for ${enumName}:`,
+          response.status
+        );
       }
     } catch (error) {
       console.error(`Error fetching enum data for ${enumName}:`, error);
     }
   };
 
-  const fetchForeignData = async (foreignResource: string, fieldName: string, foreignField: string) => {
+  const fetchForeignData = async (
+    foreignResource: string,
+    fieldName: string,
+    foreignField: string
+  ) => {
     try {
       const params = new URLSearchParams();
-      const ssid: any = sessionStorage.getItem('key');
-      params.append('queryId', 'GET_ALL');
-      params.append('session_id', ssid);
+      const ssid: any = sessionStorage.getItem("key");
+      params.append("queryId", "GET_ALL");
+      params.append("session_id", ssid);
 
       const response = await fetch(
-        `${apiConfig.API_BASE_URL}/${foreignResource.toLowerCase()}?${params.toString()}`,
+        `${apiConfig.API_BASE_URL}/${foreignResource.toLowerCase()}?` +
+          params.toString(),
         {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
         }
       );
 
@@ -122,10 +132,13 @@ const CreateCurrentaddress = () => {
         const data = await response.json();
         setForeignkeyData((prev) => ({
           ...prev,
-          [foreignResource]: data.resource
+          [foreignResource]: data.resource,
         }));
       } else {
-        console.error(`Error fetching foreign data for ${fieldName}:`, response.status);
+        console.error(
+          `Error fetching foreign data for ${fieldName}:`,
+          response.status
+        );
       }
     } catch (error) {
       console.error(`Error fetching foreign data for ${fieldName}:`, error);
@@ -136,21 +149,23 @@ const CreateCurrentaddress = () => {
     const params = new URLSearchParams();
     const jsonString = JSON.stringify(dataToSave);
     const base64Encoded = btoa(jsonString);
-    params.append('resource', base64Encoded);
-    const ssid: any = sessionStorage.getItem('key');
-    params.append('session_id', ssid);
+    params.append("resource", base64Encoded);
+    const ssid: any = sessionStorage.getItem("key");
+    params.append("session_id", ssid);
 
-    const response = await fetch(apiUrl + `?${params.toString()}`, {
-      method: 'POST',
+    const response = await fetch(apiUrl + `?` + params.toString(), {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
 
     if (response.ok) {
       setShowToast(true);
       setTimeout(() => setShowToast(false), 3000);
-      setDataToSave({ applicantid: "nil" });
+      sessionStorage.setItem("current_address", JSON.stringify(dataToSave));
+
+      setDataToSave({});
     }
   };
 
@@ -161,19 +176,26 @@ const CreateCurrentaddress = () => {
   return (
     <div>
       <div>
-        <h4 className='mt-5 fs-5'> Current address </h4>
+        <h4 className="mt-5 fs-5"> Current address </h4>
       </div>
 
       <div className="container mt-4">
         <div className="row">
           {fields.map((field, index) => {
-            if (field.name !== 'id' && field.name !== 'applicant_id' && !regex.test(field.name)) {
+            if (
+              field.name !== "id" &&
+              field.name != "applicant_id" &&
+              !regex.test(field.name)
+            ) {
               return (
                 <div key={index} className="col-md-6 mb-2">
                   {field.foreign ? (
                     <>
                       <label>
-                        {field.required && <span style={{ color: 'red' }}>*</span>} {customFieldLabels[field.name]}
+                        {field.required && (
+                          <span style={{ color: "red" }}>*</span>
+                        )}{" "}
+                        {customFieldLabels[field.name] || field.name}
                       </label>
                       <div className="dropdown">
                         <button
@@ -185,42 +207,72 @@ const CreateCurrentaddress = () => {
                           aria-expanded="false"
                         >
                           {dataToSave[field.name]
-                            ? (foreignkeyData[field.foreign]?.find((item) => item[field.foreign_field] === dataToSave[field.name])?.[field.foreign_field])
+                            ? foreignkeyData[field.foreign]?.find(
+                                (item) =>
+                                  item[field.foreign_field] ===
+                                  dataToSave[field.name]
+                              )?.[field.foreign_field]
                             : `Select ${field.name}`}
                         </button>
-                        <div className="dropdown-menu w-100" aria-labelledby={`dropdownMenu-${field.name}`}>
+                        <div
+                          className="dropdown-menu w-100"
+                          aria-labelledby={`dropdownMenu-${field.name}`}
+                        >
                           <input
                             type="text"
                             className="form-control mb-2"
                             placeholder={`Search ${field.name}`}
-                            value={searchQueries[field.name] || ''}
-                            onChange={(e) => handleSearchChange(field.name, e.target.value)}
+                            value={searchQueries[field.name] || ""}
+                            onChange={(e) =>
+                              handleSearchChange(field.name, e.target.value)
+                            }
                           />
-                          {(foreignkeyData[field.foreign] || []).filter(option =>
-                            option[field.foreign_field]?.toLowerCase()?.includes((searchQueries[field.name] || '').toLowerCase())
-                          ).map((option, i) => (
-                            <button
-                              key={i}
-                              className="dropdown-item"
-                              type="button"
-                              onClick={() => setDataToSave({ ...dataToSave, [field.name]: option[field.foreign_field] })}
-                            >
-                              {option[field.foreign_field]}
-                            </button>
-                          ))}
+                          {(foreignkeyData[field.foreign] || [])
+                            .filter((option) =>
+                              option[field.foreign_field]
+                                ?.toLowerCase()
+                                ?.includes(
+                                  (
+                                    searchQueries[field.name] || ""
+                                  ).toLowerCase()
+                                )
+                            )
+                            .map((option, i) => (
+                              <button
+                                key={i}
+                                className="dropdown-item"
+                                type="button"
+                                onClick={() =>
+                                  setDataToSave({
+                                    ...dataToSave,
+                                    [field.name]: option[field.foreign_field],
+                                  })
+                                }
+                              >
+                                {option[field.foreign_field]}
+                              </button>
+                            ))}
                         </div>
                       </div>
                     </>
                   ) : field.isEnum === true ? (
                     <>
                       <label>
-                        {field.required && <span style={{ color: 'red' }}>*</span>} {customFieldLabels[field.name]}
+                        {field.required && (
+                          <span style={{ color: "red" }}>*</span>
+                        )}{" "}
+                        {customFieldLabels[field.name] || field.name}
                       </label>
                       <select
                         name={field.name}
                         required={field.required}
-                        value={dataToSave[field.name] || ''}
-                        onChange={(e) => setDataToSave({ ...dataToSave, [e.target.name]: e.target.value })}
+                        value={dataToSave[field.name] || ""}
+                        onChange={(e) =>
+                          setDataToSave({
+                            ...dataToSave,
+                            [e.target.name]: e.target.value,
+                          })
+                        }
                         className="form-control custom-input"
                       >
                         <option value="">Select {field.name}</option>
@@ -234,15 +286,25 @@ const CreateCurrentaddress = () => {
                   ) : (
                     <>
                       <label>
-                        {field.required && <span style={{ color: 'red' }}>*</span>} {customFieldLabels[field.name]}
+                        {field.required && (
+                          <span style={{ color: "red" }}>*</span>
+                        )}{" "}
+                        {customFieldLabels[field.name] || field.name}
                       </label>
                       <input
                         type={field.type}
                         name={field.name}
                         required={field.required}
-                        placeholder={customFieldLabels[field.name] || field.name}
-                        value={dataToSave[field.name] || ''}
-                        onChange={(e) => setDataToSave({ ...dataToSave, [e.target.name]: e.target.value })}
+                        placeholder={
+                          customFieldLabels[field.name] || field.name
+                        }
+                        value={dataToSave[field.name] || ""}
+                        onChange={(e) =>
+                          setDataToSave({
+                            ...dataToSave,
+                            [e.target.name]: e.target.value,
+                          })
+                        }
                         className="form-control custom-input"
                       />
                     </>
@@ -253,10 +315,16 @@ const CreateCurrentaddress = () => {
             return null;
           })}
         </div>
-          <div className='d-flex justify-content-end'>
-        <button id='save_button' className="btn btn-success mt-4" onClick={handleCreate}>
-          Save
-        </button></div>
+
+        <div className="d-flex justify-content-end">
+          <button
+            id="save_button"
+            className="btn btn-success mt-4"
+            onClick={handleCreate}
+          >
+            Save
+          </button>
+        </div>
       </div>
 
       {showToast && (
@@ -264,7 +332,12 @@ const CreateCurrentaddress = () => {
           className="toast-container position-fixed top-20 start-50 translate-middle p-3"
           style={{ zIndex: 1550 }}
         >
-          <div className="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+          <div
+            className="toast show"
+            role="alert"
+            aria-live="assertive"
+            aria-atomic="true"
+          >
             <div className="toast-header">
               <strong className="me-auto">Success</strong>
               <button
@@ -275,7 +348,9 @@ const CreateCurrentaddress = () => {
                 onClick={() => setShowToast(false)}
               ></button>
             </div>
-            <div className="toast-body text-success text-center">Created successfully!</div>
+            <div className="toast-body text-success text-center">
+              Created successfully!
+            </div>
           </div>
         </div>
       )}
