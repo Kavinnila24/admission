2ba26060034072e593fa7fbdafbbc8c4d5e04f35
dsaikiran1 <dsaikiran7787@gmail.com>
2025-06-24 15:34:27 +0530

    dashboard

--- sApp1/src/components/Dashboard.tsx
+++ sApp1/src/components/Dashboard.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='p-4'>Dashboard</div>
+    <div className='d-flex justify-content-center p-4'>Dashboard
+    <div className='mt-4'>
+      <img
+      src="aura.jpg"
+      alt="Dashboard Image"
+      width={500}
+      height={500}
+      className="rounded-lg shadow-lg"/></div>
+    </div>
   )
 }
 export default Dashboard
